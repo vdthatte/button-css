9c0c9c49df794b671ec61a4dc4f1ee9c7c962018
Vidy <vidy.thatte@gmail.com>
2019-05-13 13:43:24 -0700

    added js file to inject script

---
+++ button.js
@@ -0,0 +1,7 @@
+var newStyle = document.createElement("link");
+
+newStyle.rel = "stylesheet";
+
+newStyle.href = "https://raw.githubusercontent.com/vdthatte/button-css/master/button.css";
+
+document.getElementsByTagName("head")[0].appendChild(newStyle);
